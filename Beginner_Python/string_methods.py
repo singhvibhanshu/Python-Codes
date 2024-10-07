b134810fd719be4f1f0281f4a3d320a9fffe9f47
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2024-10-08 00:04:09 +0530

    string methods added

---
+++ Beginner_Python/string_methods.py
@@ -0,0 +1,15 @@
+# .strip(), len(), .lower(), .upper(), .spilt()
+
+# .strip() -> removes all the blank spaces from front and end
+text1 = input("Write something with blank spaces: ")
+print(text1.strip())
+
+
+
+# len() -> for finding the length of the string
+text2 = input("Enter the something to find its length: ")
+print(len(text2))
+
+# .split() -> seperate each word and put them in the list
+text3 = input("Type something: ")
+print(text3.split('.')) # bydefault its blank space
