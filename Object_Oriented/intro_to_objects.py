c0fc166e7f79bf0bf5e83ae9af45c134e19d07e9
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2024-10-09 16:57:31 +0530

    introduction to objects added

---
+++ Object_Oriented/intro_to_objects.py
@@ -0,0 +1,17 @@
+x = 5
+y = "string"
+
+# x is the instance of the class int whose value is 5
+# y is the instance of the class str whose value is string
+
+# In Python, object is pretty much anything
+
+# Whenever we create anything in Python, it is an object
+
+print(y.upper())
+
+# I don't know whether you know it or not but a method is actually what you call with a dot (.) operator like .upper() in the above case
+
+# a method is anything that you are calling an object itself
+
+# function is something that is going to take an object and apply and operation to it
