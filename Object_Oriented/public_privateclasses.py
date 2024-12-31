727cffef63c092ec501eb7f21c921f429ed8e908
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 11:25:03 +0530

    enhaced

--- Object_Oriented/public_privateclasses.py
+++ Object_Oriented/public_privateclasses.py
@@ -1,14 +1,21 @@
-# In Python, there is no such thing like private or public classes. It's kinda pseudo public or pseudo private class.
+# In Python, there are no strict access modifiers like 'private' or 'public'.
+# However, Python uses conventions to indicate whether something is intended to be private or public.
 
-class _Private: # what makes this class a private class? The underscore '_' at the very beginning makes it to behave it that.
+# Private class - Conventionally indicated with an underscore at the beginning of the class name
+class _Private:
     def __init__(self, name):
-        self.name = name
+        self.name = name  # This is an attribute to store the name for the _Private class
 
+# Public class - No underscore before the class name
 class NotPrivate:
     def __init__(self, name):
-        self.name = name
-        self.private = _Private(name)
-    def _display(self):
-        print("Hello!")
-    def display(self):
-        print("Hey!")
+        self.name = name  # This is an attribute to store the name for the NotPrivate class
+        self.private = _Private(name)  # Creating an instance of the _Private class
+
+    # Private method - Conventionally indicated with an underscore
+    def _display(self):  
+        print("Hello!")  # This is a private method and shouldn't be accessed directly (by convention)
+
+    # Public method
+    def display(self):  
+        print("Hey!")  # This is a public method and can be accessed normally
