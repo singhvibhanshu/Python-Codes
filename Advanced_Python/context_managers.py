5809cd59405f72f7737fbe0569c350d312b8b4ea
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 12:50:58 +0530

    Update context_managers.py

--- Advanced_Python/context_managers.py
+++ Advanced_Python/context_managers.py
@@ -1,19 +1,17 @@
-# problem -> when we open a file and we do a bunch of stuffs and we don't reach that file.close(). Therefore, the point is how can we make sure that we reach to that file.close() method at the end?
-# we can solve it by try and finally methods
-
 class File:
     def __init__(self, filename, method):
-        self.file = open(filename, method)
+        self.file = open(filename, method)  # Opens the file with the given filename and method ('w', 'r', etc.)
 
     def __enter__(self):
-        print("Enter")
-        return self.file
-    
-    def __exit__(self, type, value, traceback):
-        print("Exit")
-        self.file.close()
+        print("Enter")  # This method is called when entering the 'with' block
+        return self.file  # This value is returned and assigned to 'f' in the 'with' statement
 
+    def __exit__(self, type, value, traceback):
+        print("Exit")  # This method is called when the block is exited, whether normally or due to an exception
+        self.file.close()  # Ensures that the file is properly closed
+
+# Using the custom context manager
 with File("file.txt", 'w') as f:
     print("Middle")
-    f.write("Hey!")
-    raise Exception()
+    f.write("Hey!")  # Writing to the file
+    raise Exception()  # Simulating an error to test if the file gets closed even if an exception occurs
