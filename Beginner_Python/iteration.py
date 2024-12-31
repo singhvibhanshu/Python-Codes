afd21456a9177d944600d731df5630173fb3cdd5
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 10:26:00 +0530

    enhanced

--- Beginner_Python/iteration.py
+++ Beginner_Python/iteration.py
@@ -1,4 +1,19 @@
-fruits = ["apples", "banana", "mango", "pears", "strawberry", "blueberry"]
+# Iterating through a list using a for loop in Python
 
-for x in fruits:
-    print(x)
+# Defining a list of fruits
+fruits = ["apples", "banana", "mango", "pears", "strawberry", "blueberry"]
+# A list is a collection of items in a specific order, enclosed in square brackets.
+
+# Using a for loop to iterate over the list
+for x in fruits:  
+    # The loop iterates through each item in the list, assigning it to the variable 'x' one at a time.
+    print(x)  
+    # Prints each fruit in the list.
+
+# Output:
+# apples
+# banana
+# mango
+# pears
+# strawberry
+# blueberry
