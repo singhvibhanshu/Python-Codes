5716e3d5e22c8c748736b83a2b4e1f71d6deafa2
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 11:59:35 +0530

    enhanced

--- Intermediate_Python/namedTuple.py
+++ Intermediate_Python/namedTuple.py
@@ -1,9 +1,23 @@
 import collections
 from collections import namedtuple
 
-Point = namedtuple("Point", "x y z") # Point is the name of the tuple and x y z are the fields
-# it automatically divides the string x y z into different segments (means it breaks into segments whenever it counter a gap/space) 
-Point = namedtuple("Point", ['x', 'y', 'z']) # can also be written in lists
+# Creating a named tuple 'Point' with fields 'x', 'y', 'z'
+Point = namedtuple("Point", "x y z")  # You can also use a list: ['x', 'y', 'z']
 
+# Instantiating the named tuple with values for x, y, and z
 newP = Point(3, 4, 5)
+
+# Printing the named tuple
 print(newP)
+
+# Output: Point(x=3, y=4, z=5)
+
+# Accessing the fields of a named tuple
+print(newP.x)  # Output: 3
+print(newP.y)  # Output: 4
+print(newP.z)  # Output: 5
+
+# Output: Point(x=3, y=4, z=5)
+# 3
+# 4
+# 5 
