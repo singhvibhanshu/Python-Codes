1d0e74a4b2aea8aa54b5c151f24faafa6ebc2153
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 11:35:22 +0530

    enhanced

--- Intermediate_Python/filter_function.py
+++ Intermediate_Python/filter_function.py
@@ -1,12 +1,15 @@
-# filter function is very similar to map function
-
+# Function to add 7 to a number
 def add7(x):
     return x + 7
 
+# Function to check if a number is odd
 def isOdd(x):
-    return x%2 != 0
+    return x % 2 != 0  # returns True if x is odd, False otherwise
 
+# Input list
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-b = list(filter(isOdd, a)) # here again, "list" is a keyword and filter function is taking the two arguments. First is the function (isOdd in this case) and a list (a in this case).
 
-print(b)
+# Using filter to keep only odd numbers from the list
+b = list(filter(isOdd, a))  # filter function filters based on the condition provided by isOdd
+
+print(b)  # Output: [1, 3, 5, 7, 9]
