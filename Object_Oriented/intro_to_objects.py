c63f48dae96078c8e0da710a1b58a3dacb43c864
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 11:24:21 +0530

    enhanced

--- Object_Oriented/intro_to_objects.py
+++ Object_Oriented/intro_to_objects.py
@@ -1,17 +1,10 @@
-x = 5
-y = "string"
+# Assigning values to variables x and y
+x = 5  # x is an integer (instance of the int class) with a value of 5
+y = "string"  # y is a string (instance of the str class) with a value of "string"
 
-# x is the instance of the class int whose value is 5
-# y is the instance of the class str whose value is string
+# In Python, everything is an object, including variables like x and y.
+# When we say an object is an instance of a class, we are referring to the fact that the object is created from that class.
 
-# In Python, object is pretty much anything
-
-# Whenever we create anything in Python, it is an object
-
-print(y.upper())
-
-# I don't know whether you know it or not but a method is actually what you call with a dot (.) operator like .upper() in the above case
-
-# a method is anything that you are calling an object itself
-
-# function is something that is going to take an object and apply and operation to it
+# Demonstrating a method call on the string object 'y'
+print(y.upper())  # Calling the 'upper' method on the string object 'y', which converts the string to uppercase
+# Output: STRING
