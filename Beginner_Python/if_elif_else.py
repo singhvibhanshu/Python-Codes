cfb7c7a96561da14ceb376556e0b5b40f6979b81
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 10:25:00 +0530

    enhanced

--- Beginner_Python/if_elif_else.py
+++ Beginner_Python/if_elif_else.py
@@ -1,14 +1,17 @@
 # Conditional Statements in Python
-# Conditional statements are used to execute code blocks based on specific conditions.
+# Conditional statements are used to execute specific code blocks based on given conditions.
 
 # Asking the user to input their age
 age = input("Enter your age: ")  
 # The input() function captures user input as a string.
 
-# Converting the age input to an integer for comparison
-if int(age) < 18:  
-    # The condition checks if the user's age is less than 18.
+# Checking and handling age input with conditions
+if 0 < int(age) < 18:  
+    # The condition checks if the user's age is greater than 0 and less than 18.
     print("You can't vote.")  # This block executes if the condition is True.
-else:
-    # This block executes if the condition is False (age is 18 or more).
+elif int(age) >= 18:  
+    # This block executes if the user's age is 18 or more.
     print("You can vote.")
+else:
+    # This block handles invalid age inputs (e.g., negative values or zero).
+    print("Invalid Input.")
