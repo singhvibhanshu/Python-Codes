d5d40b5cf03133e9833598917be1479537403fdf
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 14:16:00 +0530

    Update metaclasses01.py

--- Advanced_Python/metaclasses01.py
+++ Advanced_Python/metaclasses01.py
@@ -1,12 +1,10 @@
-# metaclasses are extremely complex thing.
-# here, we'll only see an overview of it.
+# Metaclasses are advanced concepts in Python, and are used to define how classes themselves behave.
+# Simply put, metaclasses define the rules for a class.
 
-# the basic idea is that a class defines the rules for an object
-# metaclass -> defines the rule for a class
-
+# Regular class definition
 class Test():
     pass
 
-print(type(Test))
-
-# now, this is something which we called as metaclasses
+# Print the type of the Test class. 
+# The type of a class is a metaclass, which is usually 'type' by default.
+print(type(Test))  # Output: <class 'type'>
