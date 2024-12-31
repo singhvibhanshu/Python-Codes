d50e252cf7d1e3bc9907905d5c7bfeb54b5dbcb1
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 10:19:05 +0530

    enhanced

--- Beginner_Python/data_types.py
+++ Beginner_Python/data_types.py
@@ -1,15 +1,20 @@
-a = 0
-b = 1
-c = -1
-d = "Vibhanshu"
-e = 'Y'
-f = 0.32
-g = True
+# Data Types in Python
+# Python provides various data types to define the nature of a variable.
 
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
-print(type(g))
+# Variable declarations with different data types
+a = 0          # Integer: Whole number (positive or negative) without a decimal point
+b = 1          # Integer
+c = -1         # Integer
+d = "Vibhanshu"  # String: A sequence of characters enclosed in double quotes
+e = 'Y'        # String: A single character can also be represented as a string (enclosed in single or double quotes)
+f = 0.32       # Float: A number with a decimal point
+g = True       # Boolean: Represents one of two values: True or False
+
+# Checking and printing the data types of the variables using the type() function
+print(type(a))  # Output: <class 'int'> (Integer)
+print(type(b))  # Output: <class 'int'> (Integer)
+print(type(c))  # Output: <class 'int'> (Integer)
+print(type(d))  # Output: <class 'str'> (String)
+print(type(e))  # Output: <class 'str'> (String)
+print(type(f))  # Output: <class 'float'> (Floating-point number)
+print(type(g))  # Output: <class 'bool'> (Boolean)
