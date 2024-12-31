d7036bdcfad2cb2993cbcf17818b71ad7b2f4d5a
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 11:26:11 +0530

    enhanced

--- Object_Oriented/staticclass_methods.py
+++ Object_Oriented/staticclass_methods.py
@@ -1,17 +1,21 @@
 class Dog:
-    dogs = []
-    x = 5 # here, x is the class variable
+    # Class variable: This variable is shared by all instances of the class
+    dogs = []  # This will store all instances of the Dog class
+    x = 5  # This is a class variable with a fixed value of 5
 
     def __init__(self, name):
+        # Instance variable: This variable is specific to each instance of the class
         self.name = name
+        # Add the instance (self) to the 'dogs' list whenever a new Dog is created
         self.dogs.append(self)
 
-    @classmethod # known as decorators
-    def num_dogs(cls): # here, cls stands for class
+    @classmethod  # This decorator indicates that the following method is a class method
+    def num_dogs(cls):  # 'cls' stands for the class itself
+        # Return the number of dogs created by accessing the 'dogs' list from the class
         return len(cls.dogs)
-    
-    @staticmethod # static methods don't need the class to be called. These are known as decorators.
+
+    @staticmethod  # This decorator marks the method as a static method
     def bark(n):
-        """""""bark n times"""""""
+        """Bark n times"""
         for _ in range(n):
             print("Bark!")
