ac273a96579fbb5f89111af1830bc0b209043796
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 14:19:31 +0530

    Update dunger_or_magic_methods.py

--- Advanced_Python/dunger_or_magic_methods.py
+++ Advanced_Python/dunger_or_magic_methods.py
@@ -1,24 +1,22 @@
 class Person:
     def __init__(self, name):
-        self.name = name
+        self.name = name  # Initialize the object with a name
 
     def __repr__(self):
-        return f"Person({self.name})"
-    
+        return f"Person({self.name})"  # String representation for debugging
+
     def __mul__(self, x):
-        if type(x) is not int:
+        if type(x) is not int:  # Validate the argument to ensure it's an integer
             raise Exception("Invalid argument, must be int")
-        self.name = self.name * x
+        self.name = self.name * x  # Multiply the name by an integer x
 
     def __call__(self, y):
-        print("Call this function", y)
+        print("Call this function", y)  # Makes the object callable like a function
 
     def __len__(self):
-        return len(self.name)
+        return len(self.name)  # Returns the length of the name attribute
 
-p = Person("Vibhanshu")
-p * 5
-print(p)
-print(len(p))
-
-# these double underscore (__) methods are known as dunder/magic methods
+p = Person("Vibhanshu")  # Create a new Person object with the name "Vibhanshu"
+p * 5  # Calls the __mul__ method, multiplying the name by 5
+print(p)  # Calls the __repr__ method, which will return the string representation of the object
+print(len(p))  # Calls the __len__ method, which returns the length of the name ("Vibhanshu")
