7fa4dd376b7d01d720b65b2e0f223ca3497fe398
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 11:09:49 +0530

    enhanced

--- Beginner_Python/try_except.py
+++ Beginner_Python/try_except.py
@@ -1,6 +1,14 @@
-inp = input("Enter the number: ")
+# Demonstrating error handling in Python using try-except blocks
+
+# Taking input from the user
+inp = input("Enter the number: ")  
+
+# Using try-except to handle errors
 try:
-    num = int(inp)
-    print("Valid Number!")
+    # Attempting to convert the input into an integer
+    num = int(inp)  
+    # If successful, this block will run, and we print "Valid Number!"
+    print("Valid Number!")  
 except:
-    print("Invalid Number!")
+    # If there is an error (e.g., input is not a valid number), this block will run
+    print("Invalid Number!")  
