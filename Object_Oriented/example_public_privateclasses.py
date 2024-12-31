92104a5506e29c3aba96ec4aeca2464cf922f983
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 11:20:57 +0530

    enhanced

--- Object_Oriented/example_public_privateclasses.py
+++ Object_Oriented/example_public_privateclasses.py
@@ -1,6 +1,12 @@
-import public_privateclasses
-from public_privateclasses import NotPrivate
+# Importing the module 'public_privateclasses' and the 'NotPrivate' class from it
+import public_privateclasses  
+from public_privateclasses import NotPrivate  
 
-test = NotPrivate("Vibhanshu")
-test.display()
-test._display()
+# Creating an instance of the 'NotPrivate' class and passing "Vibhanshu" as a parameter to the constructor
+test = NotPrivate("Vibhanshu")  
+
+# Calling the 'display()' method (public method) on the 'test' object
+test.display()  
+
+# Calling the '_display()' method (intended to be private) on the 'test' object
+test._display()  
