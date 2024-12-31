9f9524d1a10f318a1b83871d6a780fe9f0e95093
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 12:52:53 +0530

    Update decorators.py

--- Advanced_Python/decorators.py
+++ Advanced_Python/decorators.py
@@ -2,10 +2,10 @@ import time
 
 def timer(func):
     def wrapper(*args, **kwargs):
-        start = time.time()
-        rv = func()
-        total = time.time() - start
-        print("Time: ", total)
+        start = time.time()  # Start time
+        rv = func(*args, **kwargs)  # Call the original function
+        total = time.time() - start  # Calculate elapsed time
+        print("Time:", total)  # Print the time taken
         return rv
     return wrapper
 
@@ -18,5 +18,5 @@ def test():
 def test2():
     time.sleep(2)
 
-test()
-test2()
+test()   # Will print the time taken to run the loop in test()
+test2()  # Will print the time taken for 2-second sleep in test2()
