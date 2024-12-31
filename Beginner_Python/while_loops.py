2ee128a19d98b01ff70d69f9a985f6983a6774f6
Vibhanshu Singh <find.vibhanshu@gmail.com>
2024-12-31 11:11:42 +0530

    enhanced

--- Beginner_Python/while_loops.py
+++ Beginner_Python/while_loops.py
@@ -1,7 +1,14 @@
-loop = True
+# Demonstrating a while loop with a break condition
 
-while loop:
-    text = input("Write anything: ")
+# Initializing the loop condition
+loop = True  
+
+# Starting a while loop that runs as long as 'loop' is True
+while loop:  
+    # Taking input from the user
+    text = input("Write anything: ")  
+    
+    # Checking if the input is equal to the string "quit"
     if text == "quit":
-        loop = False
-        break
+        loop = False  # Setting loop to False to end the loop
+        break  # Breaking out of the while loop immediately
